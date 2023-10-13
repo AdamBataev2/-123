840ac4b4a6a865eeead4e48095447c0db0340681
VladislavPav <124609485+VladislavPav@users.noreply.github.com>
2023-10-13 17:37:33 +0400

    Добавлены файлы Windows Form

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string[] inputArray;
+        public string sumResult, productResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // считываем введенный пользователем массив чисел
+            string[] inputArray = textBoxArray.Text.Split(' ');
+
+            // забираем из класса calculation результаты
+            Calculation n1 = new Calculation();
+            Calculation n2 = new Calculation();
+
+            n1.GetSumResult(inputArray);
+            n2.GetSumResult(inputArray);
+
+
+            string s1 = n1.GetSumResult(inputArray);
+
+            string s2 = n2.GetProductResult(inputArray);
+
+            MessageBox.Show("Сумма положительных элементов: " + s1 + "; произведение чисел между минимальным и максимальным: " + s2);
+
+
+        }
+    }
+}
